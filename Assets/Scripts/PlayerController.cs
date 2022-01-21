1010e7b6363d3034dadfe4db37946b647229c6c3
Daniel Hernandez <daniel_hernandez130999@hotmail.com>
2022-01-20 21:28:57 -0600

    Se agrega la funcionalidad de salto, aun falta resolver el problema del gran salto con espacio y arriba

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 6f;
+    Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+    }
+}
