2b8ebd08dfa2b0279d19930b46c4b350a6669467
Daniel Hernandez <daniel_hernandez130999@hotmail.com>
2022-02-02 11:11:58 -0600

    Se creo el game manager y se agrego al juego

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum gameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public gameState currentGameState = gameState.menu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void GameMenu()
+    {
+
+    }
+
+    void SetGameState(gameState newGameState)
+    {
+        switch (newGameState)
+        {
+            case gameState.menu:
+                
+                break;
+            case gameState.inGame:
+
+                break;
+            case gameState.gameOver:
+
+                break;
+        }
+        currentGameState = newGameState;
+    }
+}
